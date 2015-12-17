6ce05c1e82bb26161859e3acc3aa106bdd386363
Carl Howells <carl.howells@moxiworks.com>
2015-12-17 09:51:31 -0800

    Once again, roll back all attempts to make this behave better

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -735,8 +735,14 @@ module CASServer
     end
 
     def serialize_extra_attribute(builder, key, value)
-      builder.tag! key do
-        builder.cdata! YAML.dump(value)
+      if value.kind_of?(String)
+        builder.tag! key, value
+      elsif value.kind_of?(Numeric)
+        builder.tag! key, value.to_s
+      else
+        builder.tag! key do
+          builder.cdata! value.to_yaml
+        end
       end
     end
 
